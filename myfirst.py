9e9a70b93d74929765d931a290826169beeefd14
adarshsjc <av200489adarshvish.vg@gmail.com>
2024-11-11 22:45:07 +0530

    Add files via upload

---
+++ myfirst.py
@@ -0,0 +1,21 @@
+'''
+description: a python program that calculates the sum of all even numbers betweens 1 and a given number n
+Author:Adarsh v
+
+'''
+'''
+let the fixed number 1 be name as fix
+'''
+
+
+x=int(input("Enter the number:"))
+sum=0
+i=2
+while (i<=x):
+    sum+=i
+    i+=2
+print(sum)
+    
+    
+    
+    
